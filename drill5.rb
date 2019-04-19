eba0ed949e7cc448d3d78f502a40b39606d177f9
keisukekodama <myzk.kodama@gmail.com>
2019-04-19 17:32:49 +0900

    Update drill5.rb

--- drill5.rb
+++ drill5.rb
@@ -1,3 +1,5 @@
+#整数の数字を入力し、配列の中にいくつ9が合うかを数えるプログラムを作成しましょう。
+
 def array_count9(nums)
   count = 0
   for num in nums
@@ -5,14 +7,24 @@ def array_count9(nums)
       count = count + 1
     end     
   end
+  nums << num
   puts "配列の中に9は#{count}個です"
 end
-  nums=[1,9,3,9]
-
-
-    puts "数字を入れてください"
-    num = gets.to_i
-    nums << num
-    array_count9(nums)
+  nums=[]
+    while true do
+      puts "番号を入力してください"
+      puts "0: 数字を入れる"
+      puts "1: 終了する"
+      case gets.to_i
+      when 0
+        puts "数字を入れてください"
+        num = gets.to_i
+        array_count9(nums)
+      when 1
+        exit  
+      else
+        puts '無効な値です'
+      end
+    end
+    
   
-#該当する数字（今回は9）はいくつか？
