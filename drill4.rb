4f60012ecf51a2c6a61624179b836cde213f7c73
keisukekodama <myzk.kodama@gmail.com>
2019-04-19 19:50:20 +0900

    Create drill4.rb

---
+++ drill4.rb
@@ -0,0 +1,8 @@
+# 問題文：家におしゃべりなオウムを買っています。オウムが喋っている時間が昼間だと
+# 問題は無いのですが、7時前や20時以降だと問題があります。喋っている時間が7時前や20時以降だと
+# 偽、それ以外は真と返しましょう。
+
+# 概要：
+# parrot_trouble(True, 6) → True
+# parrot_trouble(True, 7) → False
+# parrot_trouble(False, 6) → False
