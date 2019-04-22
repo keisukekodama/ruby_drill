1cb7952c0cc879cff39ea7bb13a46bf9a0d35d24
keisukekodama <myzk.kodama@gmail.com>
2019-04-22 16:51:45 +0900

    Create drill11.rb

---
+++ drill11.rb
@@ -0,0 +1,12 @@
+# 問題文：
+# 入力した1~10の範囲内であればture outside_modeがtrueで無い限り
+# それ以外は偽のプログラムを作りましょう
+# 概要：
+# 1~10の範囲内であればture outside_modeがtrueで無い限り
+# 範囲外であればfalse
+# in1to10（5、False）→True 
+# in1to10（11、False）→False 
+# in1to 10（11、True）→True
+
+
+# 模範解答
