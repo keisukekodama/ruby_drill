3cc0758daacea6e89e6d344e5ac740ad9867d7c0
keisukekodama <myzk.kodama@gmail.com>
2019-04-22 18:14:02 +0900

    Create drill02.rb

---
+++ drill02.rb
@@ -0,0 +1,9 @@
+#問題文：アタナは警官です。aとｂ二人の容疑者を別々の場所で二人の証言が
+#どちらもTrue、またはFalseであればその証言は真実です。しかしどちらかがFalseであれば
+#その証言は嘘です。
+#概要：
+# police_trouble(True, True) → True
+# police_trouble(False, False) → True
+# police_trouble(True, False) → False
+
+# 模範解答
