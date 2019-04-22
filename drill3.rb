da53cbb3905793d0304b79086a9bfa0edc35f3d9
keisukekodama <myzk.kodama@gmail.com>
2019-04-22 16:51:16 +0900

    Update drill3.rb

--- drill3.rb
+++ drill3.rb
@@ -1,6 +1,10 @@
 # 問題文：
-
-# 概要：入力した言葉に対して入力した数字が言葉のn番目文字を消す
+# 文字を入力後、数字を入力します。
+# 入力した文字に対して入力したn番目文字を消すプログラムを作りましょう
+# 概要：
 # missing_char('kitten', 1) → 'ktten'
 # missing_char('kitten', 0) → 'itten'
 # missing_char('kitten', 4) → 'kittn'
+
+
+# 模範解答
