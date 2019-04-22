1381b05c51bf8608a0eb0f7f7ef731cce0707f2d
keisukekodama <myzk.kodama@gmail.com>
2019-04-22 16:52:19 +0900

    Create drill17.rb

---
+++ drill17.rb
@@ -0,0 +1,11 @@
+# 問題文：
+# 入力した文字列に"code"が、どこかに現れるかを回数を返して数えます。
+# ただし、 'd'には任意の文字が使用可能です、 
+# 例えば"cope"と "cooe"のカウントされます。
+
+# 概要：
+# count_code('aaacodebbb') → 1
+# count_code('codexxcode') → 2
+# count_code('cozexxcope') → 2
+
+# 模範解答
