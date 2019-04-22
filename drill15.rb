6fc25ae40a43345d5f40e2f332198b5133cca0f7
keisukekodama <myzk.kodama@gmail.com>
2019-04-22 16:52:00 +0900

    Create drill15.rb

---
+++ drill15.rb
@@ -0,0 +1,10 @@
+# 問題文：
+# 入力した文字列が "xyz"の出現を含み、
+# xyzの前にピリオド（.）が続かない場合はTrueを返します。
+# したがって、 "xxyz"はカウントされますが、 "x.xyz"はカウントされません。
+# 概要：
+# xyz_there('abcxyz') → True
+# xyz_there('abc.xyz') → False
+# xyz_there('xyz.abc') → True
+
+# 模範解答
