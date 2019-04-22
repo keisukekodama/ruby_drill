a9620c8b33631e4890a3db91af43b6a79765af94
keisukekodama <myzk.kodama@gmail.com>
2019-04-22 18:14:13 +0900

    Create drill07.rb

---
+++ drill07.rb
@@ -0,0 +1,19 @@
+# 問題文：
+# 入力した文字の最初の2文字を最後尾に持ってくるプログラムを作りましょう！
+
+# 概要：
+# left2('Hello') → 'lloHe'
+# left2('java') → 'vaja'
+# left2('Hi') → 'Hi'
+
+# 模範解答
+
+def sleep_in(word)
+  
+
+  return word
+end
+
+puts "最低2文字を入力してください："
+word = gets.chomp
+sleep_in(word)
