9ed6ddb5c9bd09ef9c3fa2ab885c57a5eba20b28
keisukekodama <myzk.kodama@gmail.com>
2019-04-22 18:15:05 +0900

    Update drill11.rb

--- drill11.rb
+++ drill11.rb
@@ -1,9 +1,8 @@
 # 問題文：
-# 入力した1~10の範囲内であればture outside_modeがtrueで無い限り
+# 入力した1~10の範囲内であればtureを出力。outside_modeがtrueで無い限り
 # それ以外は偽のプログラムを作りましょう
 # 概要：
-# 1~10の範囲内であればture outside_modeがtrueで無い限り
-# 範囲外であればfalse
+# in1to10（num、outside_mode）→出力
 # in1to10（5、False）→True 
 # in1to10（11、False）→False 
 # in1to 10（11、True）→True
