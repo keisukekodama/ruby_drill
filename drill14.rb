94c7d2b30e49c9f71fa537c7ec1db8368a5ed660
keisukekodama <myzk.kodama@gmail.com>
2019-04-22 18:15:09 +0900

    Update drill14.rb

--- drill14.rb
+++ drill14.rb
@@ -1,10 +1,8 @@
 # 問題文：
-# 入力した文字がcatとdogの文字が2つで１組なら
-# 真それ以外だと偽を返す
+# 入力した文字でcatとdogの文字が2つで１組ならTrue
+# それ以外だとFalseを返す
 
 # 概要：
-# 文字列 "cat"と "dog"が、
-# 指定された文字列に同じ回数出現する場合はTrueを返します。
 # cat_dog('catdog') → True
 # cat_dog('catcat') → False
 # cat_dog('1cat1cadodog') → True
